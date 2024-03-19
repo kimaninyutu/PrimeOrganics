af02a3c4cf79ad835670c63cd4368e4018628946
nyutu <nyutukimani01@gmail.com>
2024-03-19 13:17:58 +0300

    Fixed Some Errors

--- app.py
+++ app.py
@@ -24,8 +24,7 @@ def login_required(route):
     return wrap
 
 
-@app.route('/')
-@app.route("/index", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template('index.html', email=session.get("email"))
 
